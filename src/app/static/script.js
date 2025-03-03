cb7199ad868ef88f15d3256cf05a948025a918d8
Kenzo <fernandofugihara@gmail.com>
2025-03-02 23:33:31 -0300

    face recognition init

--- src/app/static/script.js
+++ src/app/static/script.js
@@ -8,3 +8,20 @@ function showLoadingSpinner() {
       showLoadingSpinner();
     });
   }
+
+window.onload = function() {
+  var audio = document.querySelector('audio');
+  audio.play();
+};
+
+async function setupWebcam() {
+  const video = document.getElementById('webcam');
+  try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      video.srcObject = stream;
+  } catch (error) {
+      console.error('Error accessing webcam: ', error);
+  }
+}
+
+window.addEventListener('load', setupWebcam);
